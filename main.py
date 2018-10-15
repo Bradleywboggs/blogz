1c4c23c5557ed39a7e0654a8cbfd4438ebc7cf30
Bradley Boggs <bradleywboggs@gmail.com>
2018-10-15 16:37:40 -0500

    refactored to include error messages when title or body are left blank in newpost form

--- main.py
+++ main.py
@@ -9,30 +9,50 @@ db = SQLAlchemy(app)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(120))
+    title = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.String(120), nullable=False)
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/blog', methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/blog', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         title = request.form['title']
         body = request.form['body']
-        new_post = Post(title, body)
-        db.session.add(new_post)
-        db.session.commit()
+        title_error = ''
+        body_error = ''
+
+        if not title:
+            title_error = "This field is required."
+            title = ''
+        else:
+            title = title
+            title_error = title_error
+    
+        if not body:
+            body_error = "This field is required."
+            body = ''
+        else:
+            body = body
+            body_error = body_error
+        
+        if title_error or body_error:
+            return render_template('newpost.html', title=title, body=body, title_error=title_error, body_error=body_error)
+        else:    
+            new_post = Post(title, body)
+            db.session.add(new_post)
+            db.session.commit()
 
     posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
+
 @app.route('/newpost')
 def add_post():
     return render_template('newpost.html')
-
 
 if __name__ == '__main__':
     app.run()
